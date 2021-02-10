0eee909397b80e0b47d3416f17a9e335e5716da5
Grahamet <grahamet@g.cofc.edu>
2021-02-10 18:58:27 -0500

    Test run one

--- app/src/main/java/com/button_counter_graham/MainActivity.java
+++ app/src/main/java/com/button_counter_graham/MainActivity.java
@@ -3,12 +3,56 @@ package com.button_counter_graham;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button inc, dec, clear;
+    TextView tV;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inc = findViewById(R.id.button_inc);
+        dec = findViewById(R.id.button_dec);
+        tV = findViewById(R.id.textView);
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count ++;
+                tV.setText("Count: " + count);
+            }
+        });
+
+        dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count --;
+                tV.setText("Count: " + count);
+
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = 0;
+                tV.setText("Count: " + count);
+            }
+        });
     }
+
+
+
+
+
+
+
+
 }
