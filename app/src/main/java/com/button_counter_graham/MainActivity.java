476d0a6dfa1031fccc0a175e3518110a59b4029f
Grahamet <grahamet@g.cofc.edu>
2021-02-10 22:36:40 -0500

    added background colour changing

--- app/src/main/java/com/button_counter_graham/MainActivity.java
+++ app/src/main/java/com/button_counter_graham/MainActivity.java
@@ -2,16 +2,22 @@ package com.button_counter_graham;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button inc, dec, clear;
     TextView tV;
     int count;
+    int[] colour;
+    Random random = new Random();
+    View screenView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,17 @@ public class MainActivity extends AppCompatActivity {
         clear = findViewById(R.id.button_clear);
         tV = findViewById(R.id.textView);
 
+        colour = new int[] {Color.BLACK, Color.GREEN, Color.GRAY, Color.YELLOW, Color.BLUE, Color.CYAN};
+        int aryLength = colour.length;
+        screenView = findViewById(R.id.rView);
+
         inc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 count ++;
                 tV.setText("Count: " + count);
+                int rNum = random.nextInt(aryLength);
+                screenView.setBackgroundColor(colour[rNum]);
             }
         });
 
@@ -36,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 count --;
                 tV.setText("Count: " + count);
-
+                int rNum = random.nextInt(aryLength);
+                screenView.setBackgroundColor(colour[rNum]);
             }
         });
 
@@ -45,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 count = 0;
                 tV.setText("Count: " + count);
+                int rNum = random.nextInt(aryLength);
+                screenView.setBackgroundColor(colour[rNum]);
             }
         });
     }
